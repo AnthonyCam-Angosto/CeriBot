88d9dfbcec88b6d0c7c233508b4e8acdac95beb7
anthony <cam_anthony@yahoo.com>
2026-03-04 11:48:15 +0100

    mise a jour securiter coter client et autre fonctionnaliter non fini

--- ROBOT/translations/translation_fr_FR.ts
+++ ROBOT/translations/translation_fr_FR.ts
@@ -1,3 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR"/>
+<TS version="2.1" language="fr_FR">
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Bonjour</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Bonjour</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/learn_face/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>erreur durant l'apprentissage du visage</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
